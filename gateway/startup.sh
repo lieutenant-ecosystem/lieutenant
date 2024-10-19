16eeb545d6bb06d9cac98ddc3d3a79759eab36e6
Kavindu Athaudha <kavindu@kih.com.sg>
2024-10-19 17:36:33 +1300

    Changed the infrastructure so that the gateway service can act as an SSH tunnel.

---
+++ gateway/startup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Create user with a home directory and set a default shell
+adduser --disabled-password --shell /bin/bash --gecos "" "$SSH_USERNAME"
+
+# Set up SSH directory and authorized keys
+mkdir -p "/home/$SSH_USERNAME/.ssh"
+echo "$SSH_PUBLIC_KEY" > "/home/$SSH_USERNAME/.ssh/authorized_keys"
+chown -R "$SSH_USERNAME:$SSH_USERNAME" "/home/$SSH_USERNAME/.ssh"
+chmod 700 "/home/$SSH_USERNAME/.ssh"
+chmod 600 "/home/$SSH_USERNAME/.ssh/authorized_keys"
+
+# Install Cloudflared Service
+cloudflared service install "$CLOUDFLARE_TUNNEL_TOKEN"
+
+mkdir -p /run/sshd
+chmod 755 /run/sshd
+
+/usr/sbin/sshd -D
