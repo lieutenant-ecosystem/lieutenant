b630bd444c677046a4ebd58ccdb070fd5d6375b3
Kavindu Athaudha <kavindu@kih.com.sg>
2025-02-06 02:23:15 +1300

    Implemented Kubernetes

--- sergeant_service/src/common.py
+++ sergeant_service/src/common.py
@@ -10,7 +10,7 @@ class Constants(Enum):
     DATA_PATH: str = f"data/"
     LLM_YAML_PATH: str = f"{DATA_PATH}llm.yml"
     OPEN_WEBUI_PORT: str = "8080"
-    OPEN_WEBUI_URL: str = f"http://open_webui:{OPEN_WEBUI_PORT}"
+    OPEN_WEBUI_URL: str = f"http://localhost:{OPEN_WEBUI_PORT}"
 
 
 def is_production_environment() -> bool:
