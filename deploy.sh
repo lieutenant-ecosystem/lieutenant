88af4c6ef48ac7c2511a79c4949793b8ee8a9637
Kavindu Athaudha <kavindu@kih.com.sg>
2025-02-06 21:06:58 +1300

    Added a deploy script.

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+## Download files
+ENV=${1:-main}
+curl -O https://raw.githubusercontent.com/lieutenant-ecosystem/lieutenant/refs/heads/$ENV/lieutenant.yml
+curl -O https://raw.githubusercontent.com/lieutenant-ecosystem/lieutenant/refs/heads/$ENV/gateway.yml
+
+# Lieutenant
+kubectl create secret generic lieutenant-secrets \
+  --from-literal=OPENAI_API_KEY="${OPENAI_API_KEY}" \
+  --from-literal=ANTHROPIC_API_KEY="${ANTHROPIC_API_KEY}" \
+  --from-literal=PPLX_API_KEY="${PPLX_API_KEY}" \
+  --from-literal=SENTRY_DSN="${SENTRY_DSN}" \
+  --from-literal=GOOGLE_PSE_API_KEY="${GOOGLE_PSE_API_KEY}" \
+  --from-literal=GOOGLE_PSE_ENGINE_ID="${GOOGLE_PSE_ENGINE_ID}" \
+  --from-literal=MICROSOFT_CLIENT_ID="${MICROSOFT_CLIENT_ID}" \
+  --from-literal=MICROSOFT_CLIENT_SECRET="${MICROSOFT_CLIENT_SECRET}" \
+  --from-literal=MICROSOFT_CLIENT_TENANT_ID="${MICROSOFT_CLIENT_TENANT_ID}"
+kubectl apply -f lieutenant.yml
+
+# Gateway
+kubectl create secret generic gateway-secrets \
+  --from-literal=CLOUDFLARE_TUNNEL_TOKEN="${CLOUDFLARE_TUNNEL_TOKEN}" \
+  --from-literal=SSH_USERNAME="${SSH_USERNAME}" \
+  --from-literal=SSH_PUBLIC_KEY="${SSH_PUBLIC_KEY}"
+kubectl apply -f gateway.yml
